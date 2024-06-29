49143a78aa56b72598ca25193d32650b35e37439
Philip Hayes <philip@lexe.app>
2024-06-29 01:15:49 +0000

    test: fix testing script
    License: PolyForm Noncommercial License 1.0.0

--- ct.sh
+++ ct.sh
@@ -1,17 +1,16 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 export RUST_BACKTRACE=1
 
-toolchains=("nightly-2023-05-07-x86_64-unknown-linux-gnu" "nightly-2024-02-20-x86_64-unknown-linux-gnu")
 platforms=("x86_64-fortanix-unknown-sgx" "x86_64-unknown-linux-gnu")
 
-for toolchain in "${toolchains[@]}"; do
-    for platform in "${platforms[@]}"; do
-        echo "toolchain: $toolchain"
-        echo "platform: $platform"
-        echo ""
-        cargo +${toolchain} test --target  ${platform}
-        cargo +${toolchain} test --features "net,os-poll" --target  ${platform}
-        cargo +${toolchain} test --features "net,os-ext" --target  ${platform}
-        cargo +${toolchain} test --features "net,os-poll,os-ext" --target ${platform}
-    done
+for platform in "${platforms[@]}"; do
+    echo "platform: $platform"
+    echo ""
+    cargo test --target  ${platform}
+    cargo test --features "net,os-poll" --target  ${platform}
+    cargo test --features "net,os-ext" --target  ${platform}
+    cargo test --features "net,os-poll,os-ext" --target ${platform}
 done
